556baae182c62c2cb52413457fb57a86ff5c3d81
nicolus888 <nicola.edgardo@gmail.com>
2022-07-10 20:23:28 -0300

    extra: on how to pass arguments in pure nodejs

--- clases/homeworks/ejercicio 7.js
+++ clases/homeworks/ejercicio 7.js
@@ -1,5 +1,7 @@
-"use strict";
-function suma(a, b) {
-    return a + b;
+function suma1(a, b) {
+    var a = parseInt(process.argv[2]);
+    var b = parseInt(process.argv[3]);
+    console.log(a + b);
 }
+suma1();
 // Logra que esta funcion tambien concatene strings
